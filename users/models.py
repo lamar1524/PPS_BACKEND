b7ef6fd72f46d2c23117e654c47d96583a7f69ae
lamar1524 <pawel.wybrzeze@gmail.com>
2020-04-12 17:27:04 +0200

    Renaming

--- users/models.py
+++ users/models.py
@@ -4,7 +4,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
 
-from grouper import settings
+from forum import settings
 
 
 class UserManager(BaseUserManager):
